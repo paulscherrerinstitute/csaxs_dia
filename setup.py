8fffbca1af0236789aa51226d7f00ecc827aed03
Andrej Babic <andrej.babic@psi.ch>
2018-02-19 11:22:32 +0100

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name="csaxs_dia",
+      version="1.0.0",
+      maintainer="Paul Scherrer Institute",
+      maintainer_email="daq@psi.ch",
+      author="Paul Scherrer Institute",
+      author_email="daq@psi.ch",
+      description="cSAXS Eiger 9M detector integration",
+
+      license="GPL3",
+
+      packages=['csaxs_dia']
+      )
