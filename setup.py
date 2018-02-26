6b37b8ffb17dc2166051f3368a926705a261b2a2
Andrej Babic <andrej.babic@psi.ch>
2018-02-26 18:12:03 +0100

    New library version
    - Update detector_integration_api version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="csaxs_dia",
-      version="1.0.1",
+      version="1.0.2",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
