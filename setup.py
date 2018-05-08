dcc19136c07143075940ed00460ab7f39e3d6f38
Andrej Babic <andrej.babic@psi.ch>
2018-05-08 10:11:34 +0200

    New library version
    - Remove requirement to have only valid writer parameters.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="csaxs_dia",
-      version="1.0.2",
+      version="1.0.3",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
