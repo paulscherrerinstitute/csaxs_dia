e01345274b60c4729254824434c1515d84cf473b
Andrej Babic <andrej.babic@psi.ch>
2019-01-22 09:34:19 +0100

    Cache all acquisition statuses

--- csaxs_dia/status_provider.py
+++ csaxs_dia/status_provider.py
@@ -15,6 +15,8 @@ class CachedStatusProvider(object):
         self.detector_client = detector_client
 
         self._last_backend_status = None
+        self._last_writer_status = None
+        self._last_detector_status = None
 
     def get_status_details(self):
         _audit_logger.info("Getting status details.")
@@ -25,14 +27,15 @@ class CachedStatusProvider(object):
                 if self.writer_client.is_client_enabled() else ClientDisableWrapper.STATUS_DISABLED
         except:
             writer_status = IntegrationStatus.COMPONENT_NOT_RESPONDING.value
+        self._last_writer_status = writer_status
 
         _audit_logger.info("backend_client.get_status()")
         try:
             backend_status = self.backend_client.get_status() \
                 if self.backend_client.is_client_enabled() else ClientDisableWrapper.STATUS_DISABLED
-            self._last_backend_status = backend_status
         except:
             backend_status = IntegrationStatus.COMPONENT_NOT_RESPONDING.value
+        self._last_backend_status = backend_status
 
         _audit_logger.info("detector_client.get_status()")
         try:
@@ -40,6 +43,7 @@ class CachedStatusProvider(object):
                 if self.detector_client.is_client_enabled() else ClientDisableWrapper.STATUS_DISABLED
         except:
             detector_status = IntegrationStatus.COMPONENT_NOT_RESPONDING.value
+        self._last_detector_status = detector_status
 
         _logger.debug("Detailed status requested:\nWriter: %s\nBackend: %s\nDetector: %s",
                       writer_status, backend_status, detector_status)
@@ -51,35 +55,10 @@ class CachedStatusProvider(object):
     def get_cached_status_details(self):
         _audit_logger.info("Getting cached status details.")
 
-        if not self._last_backend_status or \
-                self._last_backend_status != IntegrationStatus.COMPONENT_NOT_RESPONDING.value:
+        if not self._last_writer_status or not self._last_detector_status or not self._last_backend_status:
+            _logger.info("No cached status. Requesting real status.")
+            return self.get_status_details()
 
-            _logger.info("Cached backend status invalid %s. Trying to get real status." % self._last_backend_status)
-
-            result = self.get_status_details()
-            self._last_backend_status = result["backend"]
-
-            return result
-
-        _audit_logger.info("writer_client.get_status()")
-        try:
-            writer_status = self.writer_client.get_status() \
-                if self.writer_client.is_client_enabled() else ClientDisableWrapper.STATUS_DISABLED
-        except:
-            writer_status = IntegrationStatus.COMPONENT_NOT_RESPONDING.value
-
-        try:
-            detector_status = self.detector_client.get_status() \
-                if self.detector_client.is_client_enabled() else ClientDisableWrapper.STATUS_DISABLED
-        except:
-            detector_status = IntegrationStatus.COMPONENT_NOT_RESPONDING.value
-
-        _logger.debug("Detailed cached status requested:\nWriter: %s\nBackend: %s\nDetector: %s",
-                      writer_status, self._last_backend_status, detector_status)
-
-        return {"writer": writer_status,
+        return {"writer": self._last_writer_status,
                 "backend": self._last_backend_status,
-                "detector": detector_status}
-
-
-
+                "detector": self._last_detector_status}
