32ffd483b080841ba6726924d7b0564696b2d7d8
babic_a <andrej.babic@psi.ch>
2019-01-22 14:59:23 +0100

    Request only the detector status

--- csaxs_dia/status_provider.py
+++ csaxs_dia/status_provider.py
@@ -1,8 +1,9 @@
 from logging import getLogger
 
 from detector_integration_api.utils import ClientDisableWrapper
-
+from time import time
 from csaxs_dia.validation_eiger9m import IntegrationStatus
+from csaxs_dia.detector_client import EigerClientWrapper
 
 _audit_logger = getLogger("audit_trail")
 _logger = getLogger(__name__)
@@ -14,11 +15,14 @@ class CachedStatusProvider(object):
         self.writer_client = writer_client
         self.detector_client = detector_client
 
+        self.eiger = EigerClientWrapper()
+
         self._last_backend_status = None
         self._last_writer_status = None
         self._last_detector_status = None
 
     def get_status_details(self):
+
         _audit_logger.info("Getting status details.")
 
         _audit_logger.info("writer_client.get_status()")
@@ -29,21 +33,9 @@ class CachedStatusProvider(object):
             writer_status = IntegrationStatus.COMPONENT_NOT_RESPONDING.value
         self._last_writer_status = writer_status
 
-        _audit_logger.info("backend_client.get_status()")
-        try:
-            backend_status = self.backend_client.get_status() \
-                if self.backend_client.is_client_enabled() else ClientDisableWrapper.STATUS_DISABLED
-        except:
-            backend_status = IntegrationStatus.COMPONENT_NOT_RESPONDING.value
-        self._last_backend_status = backend_status
-
-        _audit_logger.info("detector_client.get_status()")
-        try:
-            detector_status = self.detector_client.get_status() \
-                if self.detector_client.is_client_enabled() else ClientDisableWrapper.STATUS_DISABLED
-        except:
-            detector_status = IntegrationStatus.COMPONENT_NOT_RESPONDING.value
-        self._last_detector_status = detector_status
+        # TODO: Remove this once we are sure this statuses are not needed to operate the detector.
+        backend_status = None
+        detector_status = None
 
         _logger.debug("Detailed status requested:\nWriter: %s\nBackend: %s\nDetector: %s",
                       writer_status, backend_status, detector_status)
@@ -52,13 +44,3 @@ class CachedStatusProvider(object):
                 "backend": backend_status,
                 "detector": detector_status}
 
-    def get_cached_status_details(self):
-        _audit_logger.info("Getting cached status details.")
-
-        if not self._last_writer_status or not self._last_detector_status or not self._last_backend_status:
-            _logger.info("No cached status. Requesting real status.")
-            return self.get_status_details()
-
-        return {"writer": self._last_writer_status,
-                "backend": self._last_backend_status,
-                "detector": self._last_detector_status}
